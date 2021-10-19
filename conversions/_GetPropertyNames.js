659cfce59a8e3aa263416d125bc65fa1987a5012
rmcintosh <rmcintosh@comptia.org>
2021-10-19 09:21:42 -0600

    [minor] Added a property names conversion

---
+++ conversions/_GetPropertyNames.js
@@ -0,0 +1,27 @@
+/**
+* This factory produces a worker function that returns an array of object keys
+* if the value is an object. Otherwise returns "not an object".
+* @factory
+*/
+function _GetPropertyNames(
+    errors
+) {
+
+
+    return GetPropertyValue;
+
+    /**
+    * @worker
+    *   @type {converter}
+    *   @param {object} value The object to get the properties from
+    *   @returns {mixed}
+    */
+    function GetPropertyNames(value) {
+        return typeof value === "object"
+            && Object.keys(value)
+            || [
+                null
+                , `${errors.test.client.conversions.not_object} GetKeys`
+            ]
+    }
+}
